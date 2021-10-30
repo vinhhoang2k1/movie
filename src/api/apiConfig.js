21bc5ceb698c49a9b95a332fe767d7bfdf3fdaea
hoangVinh <20200129@eaut.edu.vn>
2021-10-30 18:24:44 +0700

    initial page

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: 'e5052334875e031f5dc6022e03554de8',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
